e2aaf1c2ff53c269cfb772c6a864ccaa1d5094fa
Kuasynei <Kuasynei@users.noreply.github.com>
2018-01-26 23:21:46 -0500

    Modified Game Manager very slightly to prepare for player turns.

--- GGJ2018/Assets/Scripts/Game/GameManager.cs
+++ GGJ2018/Assets/Scripts/Game/GameManager.cs
@@ -1,9 +1,33 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+
+    public GameObject tileObjectTest;
+	int playerTurn = 0; //The "Nobody's Turn" state.
+
 	// Use this for initialization
 	void Start () {
+		
+	}
+
+	void FixedUpdate() {
+		if (playerTurn == 1) {
+			
+		} 
+		else if (playerTurn >= 2) 
+		{
+			
+		}
+	}
+		
+	public void nextTurn (){
+		//Cycle the number up to 4-
+		if (playerTurn < 4)
+			playerTurn++;
+		else //and back to one.
+			playerTurn = 1;
 	}
 }
